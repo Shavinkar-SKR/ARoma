d01a46ad150996f056d22f7eec522b4d2eca7164
Sheshanthh <sheshanthvythilingam22@gmail.com>
2025-02-12 20:45:17 +0530

    checkout page final commit

--- Backend/src/routes/dist/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var orderController_1 = require("../controllers/orderController");
-var router = express_1["default"].Router();
-// Route to place order
-router.post("/place-order", orderController_1.placeOrder);
-exports["default"] = router;
