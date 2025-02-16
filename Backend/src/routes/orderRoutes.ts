2a60b93f81568876eb21958e5df185d9b1917fb4
Sheshanthh <sheshanthvythilingam22@gmail.com>
2025-02-16 21:10:17 +0530

    working backend

--- Backend/src/routes/orderRoutes.ts
+++ Backend/src/routes/orderRoutes.ts
@@ -1,9 +1,10 @@
-//import * as express from "express";
+import * as express from "express";
 import { placeOrder } from "../controllers/orderController";
-import { getOrders } from "../controllers/orderController";
+import { getOrders } from "../controllers/orderRetrival";
 const router = express.Router();
 
 // Route to place order
 router.post("/place-order", placeOrder);
+router.get("/orders",getOrders)
 
 export default router;
