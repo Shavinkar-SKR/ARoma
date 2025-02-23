036049a35f912180d87845c275eb351ec9421338
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-02-23 20:23:56 +0530

    updating files to main

--- Backend/src/routes/paymentRoutes.ts
+++ Backend/src/routes/paymentRoutes.ts
@@ -12,3 +12,16 @@
 // router.post("/create-intent", createPaymentIntent);
 
 // export default router;
+
+const express = require("express");
+const router = express.Router();
+const paymentController = require("../controllers/paymentController");
+
+router.post("/pay/stripe", paymentController.processStripePayment);
+router.post(
+  "/webhook/stripe",
+  express.raw({ type: "application/json" }),
+  paymentController.stripeWebhook
+);
+
+export default router;
