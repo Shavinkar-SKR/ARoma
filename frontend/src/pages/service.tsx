14aad7b23b96b8fdbb7bddce952a96b81c4c7a7e
methulii <methuliperera888@gmail.com>
2025-03-16 13:18:39 +0530

    changes in frontend

--- frontend/src/pages/service.tsx
+++ frontend/src/pages/service.tsx
@@ -88,7 +88,7 @@ const ServiceRequest = () => {
   };
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 font-poppins">
       {/* Toast Container */}
       <ToastContainer
         position="top-right"
@@ -100,6 +100,28 @@ const ServiceRequest = () => {
         pauseOnFocusLoss
         draggable
         pauseOnHover
+        className="toast-container"
+        toastClassName="bg-white text-gray-800 shadow-lg rounded-lg border border-gray-200 p-4 m-2 max-w-sm md:max-w-lg lg:max-w-xl"
+        progressClassName="bg-green-500"
+        closeButton={({ closeToast }) => (
+          <button
+            onClick={closeToast}
+            className="text-gray-400 hover:text-gray-600 focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        )}
       />
 
       <section className="bg-gray-100 p-6">
