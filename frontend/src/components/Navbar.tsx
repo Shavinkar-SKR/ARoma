0749d86b3b7215f36f82713213eabc8d72576f5f
Wiruni <163153212+Wiruni@users.noreply.github.com>
2025-03-18 08:53:58 +0530

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -76,6 +76,15 @@ const Navbar: React.FC = () => {
           >
             FAQ
           </Link>
+          <Link
+            to="/feedback"
+            className={`block lg:inline-block hover:text-red-500 py-2 lg:py-0 ${
+              isActive("/feedback") ? "text-red-500" : ""
+            }`}
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Feedback
+          </Link>
 
           {/* Mobile Login Button */}
           <Link
