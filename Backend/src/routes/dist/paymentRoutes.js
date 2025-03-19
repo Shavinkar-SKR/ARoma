b09841de9be001ee76db3ddf26a59e487edd540a
Sheshanthh <sheshanthvythilingam22@gmail.com>
2025-03-19 15:50:44 +0530

    Payment page addeed - Shavinkar

--- Backend/src/routes/dist/paymentRoutes.js
+++ Backend/src/routes/dist/paymentRoutes.js
@@ -1,11 +1,9 @@
-// const express = require("express");
-// const router = express.Router();
-// import * as paymentController from "../controllers/paymentController";
-// router.post("/pay/stripe", paymentController.processStripePayment);
-// router.post("/split-bill", paymentController.processSplitBillPayment);
-// router.post(
-//   "/webhook/stripe",
-//   express.raw({ type: "application/json" }),
-//   paymentController.stripeWebhook
-// );
-// export default router;
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var router = express.Router();
+var paymentController = require("../controllers/paymentController");
+router.post("/pay/stripe", paymentController.processStripePayment);
+router.post("/split-bill", paymentController.processSplitBillPayment);
+router.post("/webhook/stripe", express.raw({ type: "application/json" }), paymentController.stripeWebhook);
+exports["default"] = router;
